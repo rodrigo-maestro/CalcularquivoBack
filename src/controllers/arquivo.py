56d96a5eeed9e555f4a9e46127f81fb7cc33abed
rodrigo-maestro <rodrigo.maestro98@gmail.com>
2023-07-12 16:21:17 -0300

    realizando download de arquivos zipados

--- src/controllers/arquivo.py
+++ src/controllers/arquivo.py
@@ -1,4 +1,6 @@
 import os
+import zipfile
+import io
 
 from flask import Flask, request, send_file
 from flask_restx import Api, Resource
@@ -7,12 +9,31 @@ from src.server.instance import server
 
 app, api = server.app, server.api
 
+idUser = 1
+
 @api.route('/arquivos')
 class Arquivos(Resource):
     def get(self, ):
-        file_path = os.path.join(files_dir(),'somaBasica.txt')
+        files_dir = get_files_dir()
+        pattern = f"_{idUser}.txt"
 
-        return send_file(file_path, as_attachment=True)
+        filenames = [filename for filename in os.listdir(files_dir) if filename.endswith(pattern)]
+
+        if filenames.count == 0:
+            return
+        
+        zip_buffer = io.BytesIO()
+
+        zipfile_name = f"arquivos_user_{idUser}.zip"
+
+        with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            for filename in filenames:
+                file_path = os.path.join(files_dir, filename)
+                zipf.write(file_path, arcname=filename)
+
+        zip_buffer.seek(0)
+
+        return send_file(zip_buffer, download_name=zipfile_name, as_attachment=True)        
     
     def post(self, ):
         if 'file' not in request.files:
@@ -32,30 +53,31 @@ class Arquivos(Resource):
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
-def unique_filename(filename, directory):
+def get_unique_user_filename(filename, directory):
     base_name, extension = os.path.splitext(filename)
-    counter = 0
-    unique_filename = filename
+
+    unique_filename = f"{base_name}_{idUser}{extension}"
     
+    counter = 0
     while os.path.exists(os.path.join(directory, unique_filename)):
         counter += 1
-        unique_filename = f"{base_name}{counter}{extension}"
+        unique_filename = f"{base_name}{counter}_{idUser}{extension}"
 
     return unique_filename
 
 def save_file(file):
-    target_dir = files_dir()
+    file_dir = get_files_dir()
 
-    if not os.path.exists(target_dir):
-        os.makedirs(target_dir)
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
 
-    filename = unique_filename(file.filename, target_dir)
+    filename = get_unique_user_filename(file.filename, file_dir)
 
-    file_path = os.path.join(target_dir, filename)
+    file_path = os.path.join(file_dir, filename)
 
     file.save(file_path)
 
-def files_dir():
+def get_files_dir():
     controllers_dir = os.path.dirname(os.path.abspath(__file__))
     src_dir = os.path.dirname(controllers_dir)
     files_dir = os.path.join(src_dir, app.config['UPLOAD_FOLDER'])
